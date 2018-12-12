be4d7e0161602d2b5588c677e4f745ca8e5253c4
Henrique Fernandes <henry1991.contato@gmail.com>
2018-12-12 09:36:28 -0200

    Versão 0.2 - Correções Gerais
    Correções na classe Address.php e na index.php - Comentários inclusos no código.

--- Address.php
+++ Address.php
@@ -6,6 +6,7 @@ class Address
     {
 
     }
+    //Sugestão de nomenclatura para o método get_address - Utilizar o estilo camel case, por exemplo: function getAddress()
     function get_address($cep)
     {
         /** @param string
@@ -15,10 +16,20 @@ class Address
          * @return xml
          */
 
+
+         
+	    /*
+         *Necessário criação das variáveis para exibição na index.php
+         * $logradouro, $bairro, $uf
+         */
+
         $cep = preg_replace("/[^0-9]/", "", $cep);
         $url = "http://viacep.com.br/ws$cep/xml/";
 
         $xml = simplexml_load_file($url);
+        //Logradouro, bairro, uf estão em $xml?
+        var_dump($xml);
+        exit;
         return $xml;
     }
 
--- index.php
+++ index.php
@@ -1,28 +1,45 @@
 
 <html>
 	<title> MEU CEP </title>
+	<head>
+  		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Compiled and minified CSS -->
+    	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	</head>
 	<body> 
-		<form action="idex.php" method="post">
+		<!-- Nome do arquivo está incorreto, deveria ser action="index.php" - anterior: action="idex.php"-->
+		<form action="index.php" method="post">
 		<label> Insira o CEP: </label>
 		<input type="text" name="cep">
 		<input type="submit" value="Enviar">
 	</body>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </html>
 
 <?php
 
-//Deve-se usar include para chamada do método get_address na classe Address
+//Deve-se usar include para chamada do método get_address e criação do objeto na classe Address
 include_once('Address.php');
 
 if (!empty($_POST['cep'])) {
 
 	$cep = $_POST['cep'];
 	//Chamada ao método get_address está incorreta, deveria ser utilizada com o $this->
-	$address = ($this->get_address($cp));
+	//Variável passada como parâmetro deveria ser $cep e não $cp
+	//Deve-se criar o Objeto antes de iniciar a chamada ao método get_address
+	//Não é necessário incluir '()', pois a construct não possui argumentos e não realiza nenhuma operação
+	$address = new Adress;
+	$address->get_address($cep);
+	/*Também pode ser utilizada desta forma:
+	 * $address = (new Address)->get_address($cep);	
+	 * Tornando o código mais limpo e de fácil entendimento
+	 */
 
 	echo "<br><br>CEP Informado: $cep<br>";
-	echo "Rua: $addres->logradoro<br>";
+	//Varável $addres escrita errada - faltando 's' no final, deveria ser $address
+	echo "Rua: $address->logradoro<br>";
 	echo "Bairro: $address->bairro<br>";
+	//Varável $adress escrita errada - faltando 'd', deveria ser $address
 	echo "Estado: $adress->uf<br>";
 }
 
